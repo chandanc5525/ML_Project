923af3467e766ff2bb3b8cfa1ec76f3f3281d1a0
Chandan Chaudhari <110313705+chandanc5525@users.noreply.github.com>
2023-05-01 02:05:45 +0530

    mongodb file path added

---
+++ data_dump.py
@@ -0,0 +1,17 @@
+import pymongo
+import pandas as pd
+import json 
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://MachineLearning:ganesh22@atlascluster.nu0ydew.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
